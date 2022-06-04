c38d3a63096904c4d5c4debb212750ea0d3d317a
Jagdish Patel <jagdish.j.ptl@gmail.com>
2022-06-04 10:05:24 +0530

    added test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JagdishJP\SBIPay\Tests\Feature;
+
+use Artisan;
+use JagdishJP\SBIPay\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testTheApplicationReturnsASuccessfulResponse()
+    {
+        //Artisan::call('route:list', []);
+        $response = $this->get(route('sbi-pay.payment.initiate', [123123, 1, 'test']));
+        $response->assertStatus(404);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JagdishJP\SBIPay\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \JagdishJP\SBIPay\SBIPayServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+    }
+}
